a990a36224716d2f62fa19516ee6cff7a462e45d
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-01 13:54:15 -0400

    Adding the updated BonusQuestion file which uses the constructor and a method.

--- src/bonuspackage/BonusQuestion.java
+++ src/bonuspackage/BonusQuestion.java
@@ -9,5 +9,17 @@ package bonuspackage;
  */
 public class BonusQuestion {
 
-    
+    /**
+     * This is the main method
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create an object
+        Question greetingOne = new Question("Hey there!");
+        System.out.println("The greeting is: " + greetingOne.greetings);
+        
+        //use the method
+        greetingOne.setGreeting("Hello!");
+        System.out.println("The greeting after being modified is: " + greetingOne.greetings);
+    }
 }
