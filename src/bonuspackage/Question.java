2e2689454271e8d372125695352de70e366abe2b
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-01 13:54:37 -0400

    Adding the updated Question file which creates the constructor and a method.

--- src/bonuspackage/Question.java
+++ src/bonuspackage/Question.java
@@ -8,5 +8,22 @@ package bonuspackage;
  * @author lisa-
  */
 public class Question {
+    //Creating an attribute
+    String greetings;
     
+    /**
+     * This is the constructor with greetings as a parameter
+     * @param greetings 
+     */
+    public Question(String greetings){
+        this.greetings = greetings;
+    }
+    
+    /**
+     * This mutator allows to set a greeting.
+     * @param greetings 
+     */
+    void setGreeting(String greetings){
+        this.greetings = greetings;
+    }
 }
